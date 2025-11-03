4e564cecc6ab9057847fc6b3daa638dace4a14cc
Bibhishan123 <birajdarbibhishan5@gmail.com>
2025-11-03 23:05:55 +0530

    Questions

--- Days/031125/qn2.py
+++ Days/031125/qn2.py
@@ -1,15 +1,40 @@
-numbers_input = input("Enter the integers separated by spaces: ")
+'''
+# beginning 
+num_sep_by_space = input('Numbers (Space separated):') # '10 20 30'
+num_str_list = num_sep_by_space.split() #['10', '20', '30']
+nums = []
+for num_str in num_str_list:
+    num = int(num_str)
+    nums.append(num)
+print(nums) # [10, 20, 30]
 
-numbers_list = [int(num) for num in numbers_input.split()]
+total = 0
+for num in nums:
+    total += num 
+# end beginning 
+'''
 
-numbers_sum = sum(numbers_list)
-numbers_avg = numbers_sum/len(numbers_list)
+# easy
+nums = [int(num_str) for num_str in input('Numbers (Space separated):').split()]
+total = sum(nums)
+# end easy 
 
-print(numbers_list)
-print(numbers_sum)
-print(numbers_avg)
+avg = total / len(nums) # 60 / 3 = 20
 
-with open('qn02_data.txt', 'w') as output_file:
-    output_file.write(f'list: {numbers_list}\n')
-    output_file.write(f'sum: {numbers_sum}\n')
-    output_file.write(f'average: {numbers_sum}\n')
+print(f'sum:{total}')
+print(f'average:{avg}')
+
+print('To/From file...')
+
+with open('numbers_data.txt', 'w') as out_file:
+    out_file.write(f'numbers list: {nums}\n')
+    out_file.write(f'sum: {total}\n')
+    out_file.write(f'average: {avg}\n')
+
+with open('numbers_data.txt', 'r') as in_file:
+    line1 = in_file.readline()
+    line2 = in_file.readline()
+    line3 = in_file.readline()
+    print(line1, end='')
+    print(line2, end='')
+    print(line3, end='')
