77414cfe42ede2bce42a1290036fb383842d5553
Bibhishan123 <birajdarbibhishan5@gmail.com>
2025-11-03 23:25:09 +0530

    Questions

---
+++ Days/031125/qn3.py
@@ -0,0 +1,21 @@
+sentence = input("Enter a sentence: ")
+
+words_list = sentence.split()
+upper_words = []
+
+for word in words_list:
+    upper_words.append(word.upper)
+
+words_tuple=tuple(upper_words)
+
+with open('sentence_data.txt', 'w') as out_file:
+    out_file.write(f'List: {words_list}\n')
+    out_file.write(f'Tuple: {words_tuple}\n')
+
+with open('sentence_data.txt', 'r') as in_file:
+    line1 = in_file.readline()
+    line2 = in_file.readline()
+   
+    print(line1, end='')
+    print(line2, end='')
+   
