3ecfeca5aa6d042ac38ed280abae7fd101daa1ef
Bibhishan123 <birajdarbibhishan5@gmail.com>
2025-11-04 12:25:03 +0530

    logging- rich app dev

--- Days/251104/app_json/repo.py
+++ Days/251104/app_json/repo.py
@@ -1,17 +1,28 @@
-employees = [] #employee is object of attr (id, name, job_title, salary)
+import db_json as db 
+from log import logging 
+
+employees = db.read_employees() #employee is object of attr (id, name, job_title, salary)
 # list of objects -> list of dict -> save to file
 # read from file -> list of dict -> list of objs
 
 def add_employee(employee):
-    employees.append(employee)
-    print('Employee Added Successfully')
+    try:
+        employees.append(employee)
+        logging.debug(f'Employee {employee} added to list')
+        db.write_employees(employees)
+        logging.debug(f'Employees written to the file')
+        logging.info('Employee Added Successfully')
+    except Exception as ex:
+        logging.exception(f'Employee Added Failed. {ex}')
 
 def search_employee(id): 
     I = 0
     for employee in employees: 
         if employee.id == id:
+            logging.info('Employee Exist')
             return I
         I += 1
+    logging.info('Employee Not xist')
     return -1 
  
 def update_employee(id, salary):
@@ -19,14 +30,16 @@ def update_employee(id, salary):
     if index != -1:
         employee = employees[index]
         employee.salary = salary
-        print('Employee Updated Successfully')
+        db.write_employees(employees)
+        logging.info('Employee Updated Successfully')
     else: 
-        print('Employee Not Found.')
+        logging.info('Employee Not Found.')
 
 def delete_employee(id):
     index = search_employee(id) 
     if index != -1:
         employees.pop(index)
-        print('Employee Deleted Successfully')
+        db.write_employees(employees)
+        logging.info('Employee Deleted Successfully')
     else: 
-        print('Employee Not Found.')    
+        logging.info('Employee Not Found.')
