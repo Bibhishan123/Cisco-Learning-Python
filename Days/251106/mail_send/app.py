42757e1b0af9c5be3efcdef9b342d447aa790dab
Bibhishan123 <birajdarbibhishan5@gmail.com>
2025-11-06 10:38:43 +0530

    Mail sending with attachment

--- Days/251106/mail_send/app.py
+++ Days/251106/mail_send/app.py
@@ -1,4 +1,8 @@
-from mail import send_gmail
+dir_path = r'C:\Users\bbirajda\Downloads' 
+attachments = [
+    f'{dir_path}/IMG-20251024-WA0119.jpg',
+]
+from mail import send_gmail_attach
 from config import to_address 
-result = send_gmail(to_address, "Maheswaran - Test Subject from pystud19 - 06-11-2025", "Hello from Python!")
+result = send_gmail_attach(to_address, "First Day in Cisco - 06-11-2025 - Photo", "Hello from Bibhishan!", attachments)
 print("Mail sent successfully?" , result)
